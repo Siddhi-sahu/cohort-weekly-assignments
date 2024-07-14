c60e6529cccaf1e83934e0dfd1efeace3fbcefd4
Siddhi sahu <himanisahu739@gmail.com>
2024-07-14 11:30:41 +0530

    adding solutions

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,78 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const { User, Admin, Course } = require("../db");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await Admin.create({
+    username,
+    password,
+  });
+
+  res.json({
+    message: "Admin created successfully",
+  });
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await User.find({
+    username,
+    password,
+  });
+  if (user) {
+    const token = jwt.sign({ username }, JWT_SECRET);
+
+    res.json({ token });
+  } else {
+    res.json({
+      msg: "Wrong username and password",
+    });
+  }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageLink = req.body.imageLink;
+  const price = req.body.price;
+
+  const newCourse = await Course.create({
+    title,
+    description,
+    imageLink,
+    price,
+  });
+
+  res.json({
+    msg: "Course Created successfully",
+    courseId: newCourse._id,
+  });
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+
+  const courses = await Course.find({});
+
+  if (courses) {
+    res.json({ courses: courses });
+  } else {
+    res.json({
+      msg: "NO courses",
+    });
+  }
 });
 
 module.exports = router;
